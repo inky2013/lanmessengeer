c4b376e5fa6a607258d2560655d9adff8590abfc
Ethan Brews <ethanbrews77@gmail.com>
2017-10-08 14:04:37 +0100

    init

--- initializer.py
+++ initializer.py
@@ -1,13 +1,25 @@
 from functools import wraps
 import logging
 import inspect
+from sys import version_info
 
-
-def initializer(fun):
-    names, varargs, keywords, defaults = inspect.getargspec(fun)
-    @wraps(fun)
-    def wrapper(self, *args, **kargs):
-        for name, arg in dict(zip(names[1:], args)).update(kargs).items():
-            setattr(self, name, arg)
-        fun(self, *args, **kargs)
-    return wrapper
+if version_info.major == 3 and version_info.minor < 5:
+    def initializer(fun):
+        names, varargs, keywords, defaults = inspect.getargspec(fun)
+        @wraps(fun)
+        def wrapper(self, *args, **kargs):
+            x =  dict(zip(names[1:], args))
+            y = x.update(kargs)
+            for name, arg in y.iteritems():
+                setattr(self, name, arg)
+            fun(self, *args, **kargs)
+        return wrapper
+else:
+    def initializer(fun):
+        names, varargs, keywords, defaults = inspect.getargspec(fun)
+        @wraps(fun)
+        def wrapper(self, *args, **kargs):
+            for name, arg in {**dict(zip(names[1:], args)), **kargs}.items():
+                setattr(self, name, arg)
+            fun(self, *args, **kargs)
+        return wrapper
